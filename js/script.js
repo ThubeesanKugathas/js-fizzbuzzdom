1c365a261fa0446218042e9c973296a362db2800
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-10 14:13:05 +0100

    style css, structure html and fix js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,26 @@
 // per i multipli di 5 Buzz 
 // per i multipli di 3 E di 5 FizzBuzz (ha la priorità negli if)
 
-let outputHtml = document.querySelector('.output');
+let outputHtml = document.querySelector('.ms_output');
 
 for (let i = 1; i <=100; i++) {
 
     // prima i multipli di entrambi per priorizzare la loro differenza
     // risulterebbe corretto anche dare 'i % 15 === 0' ma sottolineo con AND l'aggiunta di entrambi i multipli
     if (i % 3 === 0 && i % 5 === 0) {
-        outputHtml.innerHTML += 'Fizzbuzz <br/>';
+        outputHtml.innerHTML += '<div class="col-2 m-2 text-center ms_box ms_box-fizzbuzz">Fizzbuzz</div>';
     } 
     // multipli di 3 
     else if (i % 3 === 0) {
-        outputHtml.innerHTML += 'Fizz <br/>';
+        outputHtml.innerHTML += '<div class="col-2 m-2 text-center ms_box ms_box-fizz">Fizz</div>';
     }
     // multipli di 5
     else if (i % 5 === 0) {
-        outputHtml.innerHTML += 'Buzz <br/>';
+        outputHtml.innerHTML += '<div class="col-2 m-2 text-center ms_box ms_box-buzz">Buzz</div>';
     } 
     // tutti i numeri che non rientrano nei if precedenti
     else {
-        outputHtml.innerHTML += `${i} <br/>`;
+        outputHtml.innerHTML += `<div class="col-2 m-2 text-center ms_box">${i}</div>`;
     }
 }
 
